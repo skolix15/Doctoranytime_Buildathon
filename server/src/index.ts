0560ce09874306f099fba3907249b385c4da2972
Papadopoulos Eleftherios <epapadopoulos@doctoranytime.gr>
2026-05-13 16:57:16 +0300

    fixes

--- server/src/index.ts
+++ server/src/index.ts
@@ -23,7 +23,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(helmet({ contentSecurityPolicy: false }));
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ limit: '30mb' }));
 
 app.use('/api/v1/auth', authRoutes);
